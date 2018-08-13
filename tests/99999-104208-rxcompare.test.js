78183104add5828e16f1d373c5b5d5e0654a7e2b
Greg Farris <greg.t.farris@gmail.com>
2018-08-13 09:46:00 -0400

    Restructure project, add tests

---
+++ tests/99999-104208-rxcompare.test.js
@@ -0,0 +1,12 @@
+var rewire = require('rewire');
+
+//Get load in the js
+var javascript = rewire('..//99999-104208/v0.0.1/model/resource/rxcompare.js');
+
+//Load in the function
+var rxcompare = javascript.__get__("rxcompare");
+
+// noinspection BadExpressionStatementJS
+test('positive numbers', () => {
+  expect( rxcompare({"rxdata": "100 MCG DAILY"})).toBe("RESULT:RARE --SUMMARY: 100 MCG DAILY is rare within 130 presriptions on file ---FREQUENCY = 0.01");
+});
