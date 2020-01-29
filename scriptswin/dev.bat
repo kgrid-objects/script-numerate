16dcf2df0a4008577310040c12893b343e2569a3
George Meng <gqmeng.meng@gmail.com>
2020-01-29 16:24:51 -0500

    [#170826966] Updated KOs;

--- scriptswin/dev.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-java -jar activator/kgrid-activator-0.6.2.jar --kgrid.shelf.cdostore.url=filesystem:file://. %*
--- tests/99999-104208-rxcompare.test.js
+++ tests/99999-104208-rxcompare.test.js
@@ -1,7 +1,7 @@
 var rewire = require('rewire');
 
 //Get load in the js
-var javascript = rewire('../collection/99999-104208/v0.0.2/rxcompare.js');
+var javascript = rewire('../collection/99999-104208/rxcompare.js');
 
 //Load in the function
 var rxcompare = javascript.__get__("rxcompare");
